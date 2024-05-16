534266feefda78b619b46312c3a43a16ef96230d
stanleyadisenu@gmail.com <stanleyadisenu@gmail.com>
2024-05-16 22:17:59 +0000

    Fix

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'capstone',
         'USER': 'postgres',
-        'PASSWORD': '@Gravity1',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
@@ -99,8 +99,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'stanleyadisenu@gmail.com'
-EMAIL_HOST_PASSWORD = 'wxqzzsumqhiqynmp'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 # Password validation
